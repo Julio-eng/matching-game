6bb4f527efecca7362cf0316809e08911ee7c654
Julio Cesar <julio0033@hotmail.com>
2022-06-06 11:59:05 -0300

    Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+function CardGame() {
+    return /*html*/ `
+        <article class="card-game">
+            <img src="images/alura-pixel.png">
+        </article>
+    `;
+}
+
+function teste() {
+    alert("deu certo");
+}
+
+export default CardGame;
+
